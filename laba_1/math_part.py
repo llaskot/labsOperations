26390ed9a0bd1894e8eaa4c3e6309cbcbe65c0f3
Konstantin.L <llaskot@gmail.com>
2025-11-22 18:15:53 +0200

    laba 1 fixed

--- laba_1/math_part.py
+++ laba_1/math_part.py
@@ -1,102 +1,172 @@
+from typing import Callable, Any
+
 import sympy as sp
+
+from laba_1.graph_3d import build_3d_graph_swen
 
 
 class laba_1:
     def __init__(self, func_str="x**2+2*x", x_0=5.0, h0=1.0):
-        self.fc = None
-        self.fb = None
-        self.fa = None
-        self.n = None
-        self.c = None
-        self.b = None
-        self.a = None
-        self.h_final = None
-        self.x_arr = None
-        self.fx_arr = None
         self.func_str = func_str
         self.func = sp.lambdify(sp.symbols('x'), sp.sympify(func_str))
         self.x0 = x_0
         self.h = h0
+        self.swen = Swen(self.func, self.x0, self.h)
 
+    def final_print(self):
+        print(
+            f'\nFinal print:\n'
+            f'function-using number = {self.swen.n_final + 1}\n'
+            f'last step = {self.swen.h_final}\n'
+
+            f'uncertainty interval (a:b) = {self.swen.a} : {self.swen.b},\n'
+            f'f(a) = {self.swen.fa},\nf(b) = {self.swen.fb},\n'
+            f'c = {self.swen.c},\nf(c) = {self.swen.fc},\n'
+        )
+        print("k:   ", "  ".join(f"{i:>5}" for i in range(len(self.swen.all_x))))
+        print("x:   ", "  ".join(f"{v:>5}" for v in self.swen.all_x))
+        print("f(x):", "  ".join(f"{v:>5}" for v in self.swen.all_fx))
+
+    def show_graph(self):
+        build_3d_graph_swen(self.swen.all_x,
+                            self.swen.all_fx,
+                            (self.swen.a, self.swen.fa),
+                            (self.swen.b, self.swen.fb),
+                            (self.swen.c, self.swen.fc),
+                            self.func,
+                            self.func_str)
+
+
+class Swen:
+    def __init__(self, my_func: Callable[..., Any], x_0: float, h_0: float):
+        """
+        income
+        :param my_func: function to process
+        :param x_0: start point
+        :param h_0: start step
+        :param max_num: max number of steps to prevent endless
+        """
+        self.my_func = my_func
+        self.x_0 = x_0
+        self.h_0 = h_0
+        # self.max_num = max_num
+
+        """
+        outcome
+        self.all_x: all x vals
+        self.all_fx: all f(x) vals
+        self.a: start uncertainty interval
+        self.b: end uncertainty interval
+        self.fa: f(start uncertainty interval)
+        self.fb: f(end uncertainty interval)
+        self.c: inner point of uncertainty interval
+        self.fc: f(inner point of uncertainty interval)
+        self.n_final: real number of iterations
+        self.h_final: last step value
+        """
+        self.all_x: list[float] = []
+        self.all_fx: list[float] = []
+        self.a: float = 0.0
+        self.b: float = 0.0
+        self.fa: float = 0.0
+        self.fb: float = 0.0
+        self.c: float = 0.0
+        self.fc: float = 0.0
+        self.n_final: int = -1
+        self.h_final: float = 0.0
+
+    @staticmethod
+    def iter_counter_deck(func):
+        def inner(self, *args, **kwargs):
+            self.n_final += 1
+            return func(self, *args, **kwargs)
+
+        return inner
+
+    # def _iteration_print(self, x, y):
+    #     print(f'point # {self.n_final}, cur step = {self.h_final}, x{self.n_final} = {x},  f(x{self.n_final}) = {y}')
+
+    @staticmethod
+    def iteration_print(func):
+        def inner(self, x, *args, **kwargs):
+            y = func(self, x, *args, **kwargs)
+            print(
+                f"point # {self.n_final}, "
+                f"cur step = {self.h_final}, "
+                f"x{self.n_final} = {x}, "
+                f"f(x{self.n_final}) = {y}"
+            )
+            return y
+
+        return inner
+
+    @iteration_print
+    @iter_counter_deck
     def find_val(self, x):
-        return self.func(x)
+        return self.my_func(x)
 
     def swen_method(self, max_steps: int = 20):
-        self.x_arr = []
-        self.fx_arr = []
-        c = self.x0
-        h = self.h
+        c = self.x_0
+        self.h_final = self.h_0
         fc = self.find_val(c)
-        print(f'k=0, {h=}, x0 = {c}, fx0 = {fc}')
-
-        b = c + h
+        # self._iteration_print(c, fc)
+        b = c + self.h_final
         fb = self.find_val(b)
-        print(f'k=1, {h=}, x1 = {b}, fx1 = {fb}')
-
-        self.x_arr.append(c)
-        self.x_arr.append(b)
-        self.fx_arr.append(fc)
-        self.fx_arr.append(fb)
+        # self._iteration_print(b, fb)
+        self.all_x.append(c)
+        self.all_x.append(b)
+        self.all_fx.append(fc)
+        self.all_fx.append(fb)
         if fb >= fc:
-            a = c - h
+            a = c - self.h_final
             fa = self.find_val(a)
-            self.x_arr.append(a)
-            self.fx_arr.append(fa)
-            print(f'k=2, {h=}, x2 = {a}, fx2 = {fa}')
+            self.all_x.append(a)
+            self.all_fx.append(fa)
+            # self._iteration_print(a, fa)
             if fa >= fc:
-                self.a, self.b, self.c, self.n = a, b, c, 2
-                self.fa, self.fb, self.fc, self.h_final = fa, fb, fc, h
+                self.a, self.b, self.c = a, b, c
+                self.fa, self.fb, self.fc = fa, fb, fc
             else:
-                self.swen_inc(c, fc, a, fa, 2 * h, max_steps)
+                self._swen_inc(c, fc, a, fa, max_steps)
         else:
-            self.swen_dec(c, fc, b, fb, h * 2, max_steps)
-        print(f'uncertainty interval (a:b) = {self.a} : {self.b},\nf(a) = {self.fa},\nf(b) = {self.fb},\n'
-              f'c = {self.c},\nf(c) = {self.fc},\nfunction-using number = {self.n}')
-        print("k:   ", "  ".join(f"{i:>5}" for i in range(len(self.x_arr))))
-        print("x:   ", "  ".join(f"{v:>5}" for v in self.x_arr))
-        print("f(x):", "  ".join(f"{v:>5}" for v in self.fx_arr))
+            self._swen_dec(c, fc, b, fb, max_steps)
 
-    def swen_dec(self, a, fa, c, fc, h, max_steps):
-        n = 2
-        while n < max_steps:
-            b = c + h
+    def _swen_dec(self, a, fa, c, fc, max_steps):
+        while self.n_final < max_steps:
+            self.h_final *= 2
+            b = c + self.h_final
             fb = self.find_val(b)
-            self.x_arr.append(b)
-            self.fx_arr.append(fb)
-            print(f'k={n}, {h=}, x{n} = {b}, fx{n} = {fb}')
+            self.all_x.append(b)
+            self.all_fx.append(fb)
+            # self._iteration_print(b, fb)
+
             if fb < fc:
-                a, fa, c, fc, h = c, fc, b, fb, 2 * h
-                n += 1
+                a, fa, c, fc = c, fc, b, fb
             else:
-                self.a, self.b, self.c, self.n = a, b, c, n
-                self.fa, self.fb, self.fc, self.h_final = fa, fb, fc, h
+                self.a, self.b, self.c = a, b, c
+                self.fa, self.fb, self.fc = fa, fb, fc
                 return
 
-    def swen_inc(self, b, fb, c, fc, h, max_steps):
-        n = 3
-        while n < max_steps:
-            a = c - h
+    def _swen_inc(self, b, fb, c, fc, max_steps):
+        while self.n_final < max_steps:
+            self.h_final *= 2
+            a = c - self.h_final
             fa = self.find_val(a)
-            self.x_arr.append(a)
-            self.fx_arr.append(fa)
-            print(f'k={n}, {h=}, x{n} = {a}, fx{n} = {fa}')
+            self.all_x.append(a)
+            self.all_fx.append(fa)
+            # self._iteration_print(a, fa)
             if fa >= fc:
-                self.a, self.b, self.c, self.n, self.h_final = a, b, c, n, h
+                self.a, self.b, self.c = a, b, c
                 self.fa, self.fb, self.fc = fa, fb, fc
                 return
             else:
-                b, fb, c, fc, h = c, fc, a, fa, 2 * h
-                n += 1
+                b, fb, c, fc = c, fc, a, fa
 
 
-if __name__ == "__main__":
-    laba = None
-    func = input("input the function or press enter to use default values: ")
-    if func:
-        x0 = float(input("input x0 value: "))
-        h = float(input("input x0 value: "))
-        laba = laba_1(func, x0, h)
-    else:
-        laba = laba_1()
-    n = int(input("input max number of iterations: "))
-    laba.swen_method(n)
+# if __name__ == "__main__":
+#     # laba = laba_1(func_str="x**2+2*x", x_0=-10.0, h0=1.0)
+#     laba = laba_1()
+#
+#     laba.swen.swen_method(6)
+#     laba.final_print()
+#     laba.show_graph()
--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-
 from laba_1.graph_3d import build_3d_graph_swen
 from laba_1.math_part import laba_1
 
 
-
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-    laba = None
+def lab_1():
     func = input("input the function or press enter to use default values: ")
     if func:
         x0 = float(input("input x0 value: "))
-        h = float(input("input x0 value: "))
+        h = float(input("input h0 value: "))
         laba = laba_1(func, x0, h)
     else:
         laba = laba_1()
-    n = input("input max number of iterations: ")
+    n = input("input max number of iterations or press enter to use default value (20):  ")
+    print("\nLaboratory job # 1 in progress:\n")
     if n:
-        laba.swen_method(int(n))
+        laba.swen.swen_method(int(n))
     else:
-        laba.swen_method()
-    build_3d_graph_swen(laba.x_arr, laba.fx_arr, (laba.a, laba.fa), (laba.b, laba.fb),
-                        (laba.c, laba.fc), laba.func, laba.func_str)
+        laba.swen.swen_method()
+    laba.final_print()
+    laba.show_graph()
+
+
+if __name__ == '__main__':
+    laba_num = input('input number of LW (1, 2 is available): ')
+
+    match laba_num:
+        case '1':
+            lab_1()
+        case '2':
+            print('in progress')
+            print("Dichotomy method - 1")
